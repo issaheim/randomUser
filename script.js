0180839136360b50393ad0dbf4282d0feb9d13f7
issaheim <75672568+issaheim@users.noreply.github.com>
2021-02-15 20:24:51 +0100

    solved the assignment

---
+++ script.js
@@ -0,0 +1,41 @@
+const rootDiv = document.createElement("div");
+const userDiv = document.createElement("div");
+
+document.body.appendChild(rootDiv);
+rootDiv.appendChild(userDiv);
+
+fetch ('https://randomuser.me/api/')
+.then (function (response) {
+  return response.json();
+})
+.then (function (user) {
+  console.log(user);
+
+  printRandomUser(user);
+})
+
+
+function printRandomUser (user) {
+  console.log(user.results[0]);
+
+  let userPic = user.results[0].picture.medium;
+  let fName = user.results[0].name.first;
+  let lName = user.results[0].name.last;
+  let email = user.results[0].email;
+  let cell = user.results[0].cell;
+
+  console.log(cell, fName, email);
+
+  userDiv.insertAdjacentHTML("afterbegin", 
+
+  ` <img src="${userPic}" alt="a picture of the user"> 
+
+    <h2>${fName} ${lName}</h2>
+    
+    <ul>
+      <li>${email}</li>
+      <li>${cell}</li>
+    </ul>
+  `)
+}
+
